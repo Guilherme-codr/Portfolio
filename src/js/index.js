eda9f9bbfe44c716b98912b63571a8c566027a1a
Guilherme Lacerda <111709301+Guilherme-codr@users.noreply.github.com>
2025-01-08 20:57:06 -0300

    Add files via upload

--- src/js/index.js
+++ src/js/index.js
@@ -2,9 +2,17 @@ const botaomostrarprojetos = document.querySelector('.btn-mostrar-projetos');
 const projetosinativos  = document.querySelectorAll('.projeto:not(.ativo');
 
 botaomostrarprojetos.addEventListener('click', () => {
+    mostrarMaisProjetos();
+
+    esconderBotao();
+});
+
+function esconderBotao() {
+    botaomostrarprojetos.classList.add("remover");
+}
+
+function mostrarMaisProjetos() {
     projetosinativos.forEach(projetoinativo => {
         projetoinativo.classList.add('ativo');
     });
-
-    botaomostrarprojetos.classList.add("remover");
-});
+}
